049f655150e69ebebf03cb83ff2944eedfe9a46a
naveenraj129 <153100819+naveenraj129@users.noreply.github.com>
2024-01-25 21:35:55 +0530

    Create LargestNumber.java

---
+++ LargestNumber.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class LargestNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+        System.out.print("Enter the third number: ");
+        double num3 = scanner.nextDouble();
+
+        double largestNumber = Math.max(num1, Math.max(num2, num3));
+        System.out.println("The largest number is: " + largestNumber);
+    }
+}
