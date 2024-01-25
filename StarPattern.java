69a108c47d0dc5b61dbcbb6f578f5b65b8594361
naveenraj129 <153100819+naveenraj129@users.noreply.github.com>
2024-01-25 21:33:08 +0530

    Create StarPattern.java

---
+++ StarPattern.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class StarPattern {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int n = scanner.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j || i + j == n - 1) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
