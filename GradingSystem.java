15d37278a3993eea283b99af103ccdf9e98efbe5
naveenraj129 <153100819+naveenraj129@users.noreply.github.com>
2024-01-25 21:33:40 +0530

    Create GradingSystem.java

---
+++ GradingSystem.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class GradingSystem {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the marks: ");
+        int marks = scanner.nextInt();
+
+        if (marks > 100) {
+            System.out.println("Invalid Input");
+        } else {
+            if (marks >= 90) {
+                System.out.println("A");
+            } else if (marks >= 80) {
+                System.out.println("B");
+            } else if (marks >= 70) {
+                System.out.println("C");
+            } else if (marks >= 60) {
+                System.out.println("D");
+            } else if (marks >= 50) {
+                System.out.println("E");
+            } else {
+                System.out.println("F");
+            }
+        }
+    }
+}
