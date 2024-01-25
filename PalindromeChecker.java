85ecc6a680edce68444ec60fde6530b6b8499aff
naveenraj129 <153100819+naveenraj129@users.noreply.github.com>
2024-01-25 21:30:46 +0530

    Create PalindromeChecker

---
+++ PalindromeChecker.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class PalindromeChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = scanner.nextLine();
+
+        if (isPalindrome(str)) {
+            System.out.println(str + " is a palindrome.");
+        } else {
+            System.out.println(str + " is not a palindrome.");
+        }
+    }
+
+    private static boolean isPalindrome(String s) {
+        int n = s.length();
+        for (int i = 0; i < n / 2; i++) {
+            if (s.charAt(i) != s.charAt(n - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
